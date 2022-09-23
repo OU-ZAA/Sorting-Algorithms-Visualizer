d5e7a3a1ae7d63d8899e7d9ca02a1d06b3012420
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2022-09-23 10:11:49 +0100

    getting the generator button working

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 const arrayNumber = 42;
 const arrayContainer = document.getElementsByClassName('array-container')[0];
+const arrayGenerator = document.getElementById('generate-button');
+let array = [];
 
-document.addEventListener("DOMContentLoaded", function() {
-    // if (array.length === arrayNumber) return;
-    resetArray(); 
+document.addEventListener('DOMContentLoaded', function() {
+
+    generateNewArray(); 
+    renderArrayElements();
+
 });
 
-function resetArray() {
-    const array = [];
+arrayGenerator.addEventListener('click', function() {
+
+    generateNewArray();
+    renderNewArrayElements(); 
+
+});
+
+
+function generateNewArray() {
+
     for (let i = 0; i < arrayNumber; i++) {
-        array.push(randonIntFromInterval(5, 500));
+        array.splice(i, 1, randonIntFromInterval(5, 500));
     }
+
+}
+
+function renderArrayElements() {
+
     array.map(function(value) {
         const element = document.createElement('div');
         element.classList.add('array-bar')
         element.style.height = `${value}px`;
         arrayContainer.appendChild(element);
-    });
+    });   
+    
+}
+
+function renderNewArrayElements() {
+
+    const elements = document.getElementsByClassName("array-bar");
+
+    for (let i = 0; i < arrayNumber; i++) {
+        elements[i].style.height = `${array[i]}px`;
+    }
+    
 }
 
 function randonIntFromInterval(min, max) {
