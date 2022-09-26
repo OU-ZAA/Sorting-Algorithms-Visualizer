a6ec209a231dfa6edd17d26401438478a864dd68
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2022-09-26 13:21:44 +0100

    add normal sorting algorithm function

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const arrayNumber = 42;
 const arrayContainer = document.getElementsByClassName('array-container')[0];
 const arrayGenerator = document.getElementById('generate-button');
+const sortButton = document.getElementById('sorting-button');
 let array = [];
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    generateNewArray(); 
-    renderArrayElements();
+    generateNewArray(array); 
+    renderArrayElements(array);
 
 });
 
 arrayGenerator.addEventListener('click', function() {
 
-    generateNewArray();
-    renderNewArrayElements(); 
+    generateNewArray(array);
+    renderNewArrayElements(array); 
 
 });
 
+sortButton.addEventListener('click', function() {
+    
+    SortAlgorithm(array);
+    renderNewArrayElements(array);
+});
 
-function generateNewArray() {
+
+function generateNewArray(array) {
 
     for (let i = 0; i < arrayNumber; i++) {
         array.splice(i, 1, randonIntFromInterval(5, 500));
@@ -26,7 +33,7 @@ function generateNewArray() {
 
 }
 
-function renderArrayElements() {
+function renderArrayElements(array) {
 
     array.map(function(value) {
         const element = document.createElement('div');
@@ -37,7 +44,7 @@ function renderArrayElements() {
     
 }
 
-function renderNewArrayElements() {
+function renderNewArrayElements(array) {
 
     const elements = document.getElementsByClassName("array-bar");
 
@@ -49,4 +56,17 @@ function renderNewArrayElements() {
 
 function randonIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function SortAlgorithm(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[i]) {
+                let temp = array[i]; 
+                array[i] = array[j]; 
+                array[j] = temp;
+            }
+        }
+    }
 }
